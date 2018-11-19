ea3899b0a071084adb9d09519d65cc58515a7bb1
francisatkinson <44147570+francisatkinson@users.noreply.github.com>
2018-11-19 23:17:30 +0000

    Phone chassis, status bar, cursor
    Added phone chassis elements (speaker, camera, home button).
    
    Confgiured home button to reload iframe.
    
    Added status bar image.
    
    Custom (Chrome inspector like) cursor when on phone screen

--- app/scripts/global.js
+++ app/scripts/global.js
@@ -1,8 +1,12 @@
 $(document).ready(function() {
-	let slideCount = 0;
+
+	floorplanCount = 6;
+
 	$(".hamburger, .overlay").click(function() {
 		hamburger();
 	});
+
+	drawFloorPlans(floorplanCount);
 });
 hamburger = () => {
 	$(".overlay").fadeToggle();
@@ -10,4 +14,29 @@ hamburger = () => {
 }
 mySlideToggle = (sidebar) => {
 	sidebar.toggleClass("slideIn");
+}
+
+accordian = () => {
+
+}
+drawFloorPlans = (floors) => {
+	html = "";
+	for (i = 0; i < floors; i++) {
+		html += `
+		<div class="card">
+			<div class="cardHeader">
+				<p>
+					Card Title
+				</p>
+			</div>
+			<div class="cardLine"></div>
+			<div class="cardBody">
+				<p>
+					Card Body
+				</p>
+			</div>
+		</div>
+		`;
+	}
+	$(".body").append(html);
 }
--- scripts/global.js
+++ scripts/global.js
@@ -1,8 +1,19 @@
 $(document).ready(function() {
-	console.log("hello world");
 	appear();
+	$(".phoneButton").click(function() {
+		reset();
+	});
 });
 appear = () => {
 	$(".topHalf").css("height", "70vh");
 	$(".phone").css("top", "10%");
+}
+reset = () => {
+	setTimeout(function() {
+		$("#phone").css("opacity", "0");
+	}, 0);
+	setTimeout(function() {
+		$("#phone").attr("src", "app/index.html");
+		$("#phone").css("opacity", "1");
+	}, 250);
 }
